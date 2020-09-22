73f9b9d05a94043b6d09b5f3c0f5b770e637ed62
Daniel Gagné <dgagne@influencecommunication.com>
2020-09-22 14:07:09 -0400

    Attributes Binding

---
+++ main.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    product: 'Socks',
+    description:
+      'A sock is a piece of clothing worn on the feet and often covering the ankle or some part of the calf.',
+    image: './assets/vmSocks-green-onWhite.jpg',
+    link:
+      'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+  },
+});
